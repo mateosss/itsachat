a72a89132d436cc13fbb6adcf48e34b951f0024f
mateosss <mateodemayo@gmail.com>
2014-09-12 22:49:14 -0300

    Ahora si hace un ftp bien hecho

--- MakeFtp.sh
+++ MakeFtp.sh
@@ -3,22 +3,27 @@ echo "Descargando vsftpd..."
 sudo apt-get install vsftpd
 
 echo "Configurando ftp..."
-rm /etc/vsftpd.conf
-touch /etc/vsftpd.conf
-echo "listen=YES" >> /etc/vsftpd.conf
-echo "anonymous_enable=YES" >> /etc/vsftpd.conf
-echo "local_enable=YES" >> /etc/vsftpd.conf
-echo "dirmessage_enable=YES" >> /etc/vsftpd.conf
-echo "use_localtime=YES" >> /etc/vsftpd.conf
-echo "xferlog_enable=YES" >> /etc/vsftpd.conf
-echo "connect_from_port_20=YES" >> /etc/vsftpd.conf
-echo "secure_chroot_dir=/var/run/vsftpd/empty" >> /etc/vsftpd.conf
-echo "pam_service_name=vsftpd" >> /etc/vsftpd.conf
-echo "rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem" >> /etc/vsftpd.conf
-echo "rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key" >> /etc/vsftpd.conf
+sudo rm /etc/vsftpd.conf
+sudo touch /etc/vsftpd.conf
+sudo mkdir /var/ftp
+sudo echo "listen=YES" >> /etc/vsftpd.conf
+sudo echo "anonymous_enable=YES" >> /etc/vsftpd.conf
+sudo echo "local_enable=NO" >> /etc/vsftpd.conf
+sudo echo "anon_max_rate=104857" >> /etc/vsftpd.conf
+sudo echo "dirmessage_enable=YES" >> /etc/vsftpd.conf
+sudo echo "listen_port=21" >> /etc/vsftpd.conf
+sudo echo "anon_root=/var/ftp" >> /etc/vsftpd.conf
+sudo mount --bind $HOME /var/ftp
 
 echo "Iniciando ftp..."
 sudo service vsftpd restart
+
+echo "Creando carpetas del Chat"
+
+mkdir $HOME/ITSaChat/
+mkdir $HOME/ITSaChat/Archivos
+mkdir $HOME/ITSaChat/Interno
+mkdir $HOME/ITSaChat/Interno/Historial
 
 echo "Listo!"
 zenity --info --text "Usted ya tiene su server ftp corriendo en la direccion!\n\t\t"
